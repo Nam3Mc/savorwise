37b27915c69f9672a07057df0a77967ca709e6bc
nam3mc <moralesdreiser5@gmail.com>
2026-08-07 18:51:21 -0500

    git vite config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,32 @@
+// import { resolve } from "path";
+// import { defineConfig } from "vite";
+
+// export default defineConfig({
+//   root: "src/",
+//   envDir: "../",
+
+//   build: {
+//     outDir: "../dist",
+//     rollupOptions: {
+//       input: {
+//         main: resolve(__dirname, "src/index.html"),
+//       },
+//     },
+//   },
+// });
+
+
 import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: "src/",
-  envDir: "../",
+    root: "src",
+    envDir: "..",
 
-  build: {
-    outDir: "../dist",
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "src/index.html"),
-      },
+    build: {
+        outDir: "../dist",
+        rollupOptions: {
+            input: resolve("src/index.html"),
+        },
     },
-  },
 });
