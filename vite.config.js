1dea3b0ee7b68701510b4ae439f6e581252c64ff
Dreiser Morales <108500975+Nam3Mc@users.noreply.github.com>
2026-08-08 01:15:42 -0500

    Refactor vite.config.js to use path.resolve
    Updated paths in vite.config.js to use resolve for better compatibility.

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,22 @@
-import { resolve } from "path";
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-    root: "src/",
-    envDir: "../",
+    root: resolve(__dirname, "src"),
+    envDir: resolve(__dirname),
 
     preview: {
         host: "0.0.0.0",
+        port: 10000,
         allowedHosts: ["savorwise.onrender.com"],
     },
 
     build: {
-        outDir: "../dist",
+        outDir: resolve(__dirname, "dist"),
+        emptyOutDir: true,
+
         rollupOptions: {
-            input: {
-                main: resolve(__dirname, "src/index.html"),
-            },
+            input: resolve(__dirname, "src/index.html"),
         },
     },
 });
